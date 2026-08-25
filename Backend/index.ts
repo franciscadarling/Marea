7b5cf7a60caf46b69826bb1fa00c8ca3568bfd4e
Alan <you@example.com>
2026-08-25 15:52:25 -0300

    NuevosJsons

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,77 +1,83 @@
 import * as fs from 'fs';
 import { stringify } from 'querystring';
 
+type InfoGral=  {
+  id: number;
+  nombre: string;
+  mail: string;
+  contraseña: number;
+  genero: string;
+  edad: number;
+  foto: string;
+  paleta: string;
+  regular: boolean;
+  condiciones: string;
+  peso: number;
+  altura: number;
+}
+
+type Sintoma = {id: number;
+  fecha: string;
+  entrada: string;
+  ligero: boolean;
+  moderado: boolean;
+  fuerte: boolean;
+  masacre: boolean;
+  rojo: boolean;
+marron: boolean;
+  cambiosDeAnimo: boolean;
+  normal: boolean;
+  irritabilidad: boolean;
+  enojo: boolean;
+  ansiedad: boolean;
+  tristeza: boolean;
+  Indiferencia: boolean;
+  sinDolor: boolean;
+  colicos: boolean;
+  sensibilidadSenos: boolean;
+  migraña: boolean;
+  lumbares: boolean;
+  articulaciones: boolean;
+  vulvar: boolean;
+  tampon: boolean;
+  toallahigienica: boolean;
+  protectorDiario: boolean;
+  copa: boolean;
+  ropaInteriorPro: boolean;
+  malaMemoria: boolean;
+  nieblaMental: boolean;
+  distraccion: boolean;
+  estres: boolean;
+  sinMotivacion: boolean;
+  bajoRendimiento: boolean;
+  vitalidad: boolean;
+  ok: boolean;
+  Agotamiento: boolean;
+  cansancioFatiga: boolean;
+  Mareo: boolean;
+  Nauseas: boolean;
+  debilidadMuscular: boolean;
+  presionBaja: boolean;
+}
+type Edu = {
+  id: number;
+  articulo: number;
+  leido: boolean;
+}
+
+type Ciclo = {
+  id: number;
+  fechasDeMenstruacion: string[]
+}
+
 type tablas = {
-    infogeneral: {
-      id: number;
-      nombre: string;
-      mail: string;
-      contraseña: number;
-      genero: string;
-      edad: number;
-      foto: string;
-      paleta: string;
-      regular: boolean;
-      condiciones: string;
-      peso: number;
-      altura: number;
-    }[];
+    infogeneral: InfoGral[];
   
-    sintomas: {
-      id: number;
-      fecha: string;
-      entrada: string;
-      ligero: boolean;
-      moderado: boolean;
-      fuerte: boolean;
-      masacre: boolean;
-      rojo: boolean;
-    marron: boolean;
-      cambiosDeAnimo: boolean;
-      normal: boolean;
-      irritabilidad: boolean;
-      enojo: boolean;
-      ansiedad: boolean;
-      tristeza: boolean;
-      Indiferencia: boolean;
-      sinDolor: boolean;
-      colicos: boolean;
-      sensibilidadSenos: boolean;
-      migraña: boolean;
-      lumbares: boolean;
-      articulaciones: boolean;
-      vulvar: boolean;
-      tampon: boolean;
-      toallahigienica: boolean;
-      protectorDiario: boolean;
-      copa: boolean;
-      ropaInteriorPro: boolean;
-      malaMemoria: boolean;
-      nieblaMental: boolean;
-      distraccion: boolean;
-      estres: boolean;
-      sinMotivacion: boolean;
-      bajoRendimiento: boolean;
-      vitalidad: boolean;
-      ok: boolean;
-      Agotamiento: boolean;
-      cansancioFatiga: boolean;
-      Mareo: boolean;
-      Nauseas: boolean;
-      debilidadMuscular: boolean;
-      presionBaja: boolean;
-    }[];
+    sintomas: Sintoma[];
   
-    educacion: {
-      id: number;
-      articulo: number;
-      leido: boolean;
-    }[];
+    educacion: Edu[];
   
-    fechas: {
-      id: number;
-      fechasDeMenstruacion: string[];
-    }[];
+    fechas: Ciclo[];
   }
 const ruta: string = './tablas.json'
 const contenido: string = fs.readFileSync(ruta, 'utf8');
