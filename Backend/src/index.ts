cae7f2c0e8d7f339ada731546ba84fe86d96640c
Alan-Grebnicoff <alangrebni@gmail.com>
2026-09-01 19:33:52 -0300

    PuedoLeerPartesEspecificas

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -14,20 +14,28 @@ function leerDatosSintomas(): Sintomas {
   return JSON.parse(contenido) as Sintomas;
 }
 
-/*function leerDatosEducacion(): Educacion {
+function leerDatosEducacion(): Educacion {
+  const ruta: string = '../Data/educacion.json'
   const contenido = fs.readFileSync(ruta, 'utf8');
   return JSON.parse(contenido) as Educacion;
 }
 
 function leerDatosCiclos(): CiclosMenstruales {
+  const ruta: string = '../Data/ciclos.json'
   const contenido = fs.readFileSync(ruta, 'utf8');
   return JSON.parse(contenido) as CiclosMenstruales;
-}*/
-
-let Datitos = leerDatosUsuarios ()
-const Aleer = "mail" as keyof Users[0];
+}
+function DatoEspecifico
+<T //"No sé qué tipo de objeto me vas a mandar todavía, pero al que mandes lo voy a bautizar temporalmente como T"
+ extends //"debe cumplir con la condición de..."
+ Record<string, any>> //"Cualquier objeto que tenga llaves de tipo texto (string) y valores de cualquier tipo (any)"
+ (Tabla: T[], Aleer: keyof T, id: number) {
+  return Tabla[id]?.[Aleer];
+}
+/*let Datitos = leerDatosUsuarios ()
+const Aleer = "mail" as keyof Users[number];
 console.log(Datitos[0]?.[Aleer])
-/*
+
 let IdLeer: number= 1
 //Quiero leer el mail de la persona id 2
 for (let id: number = 0; id < Datitos.length; id++) {
@@ -38,3 +46,7 @@ for (let id: number = 0; id < Datitos.length; id++) {
     console.log (Datitos)
   }
 }*/
+
+let hola = leerDatosSintomas ()
+let info = DatoEspecifico (hola, 'fuerte', 0)
+console.log (info)
