d6da1d140afc70ae4327f3d5a0c542ee719a7824
Alan-Grebnicoff <alangrebni@gmail.com>
2026-09-05 11:59:20 -0300

    TengoDuracionnnn

--- Backend/src/lib/calculoCiclo.ts
+++ Backend/src/lib/calculoCiclo.ts
@@ -7,12 +7,17 @@ import * as fs from 'fs';
     "duracion": 4
   },
   {
+    "id": 1,
+    "inicio": "2026-07-06",
+    "duracion": 6
+  },
+  {
     "id": 0,
     "inicio": "2026-09-01",
-    "duracion": 4
+    "duracion": 5
   }
 ]*/
-function duracionCiclos (Ciclos: CiclosMenstruales, idusuario: number) : void {
+function duracionCiclos (Ciclos: CiclosMenstruales, idusuario: number) : number[] {
     let dias: number []= []
     for (let i:number =0; i<Ciclos.length; i++) {
         if (Ciclos[i]?.id===idusuario) {
@@ -20,23 +25,54 @@ function duracionCiclos (Ciclos: CiclosMenstruales, idusuario: number) : void {
         }
         else {}
     }
-    console.log (dias)
+    return dias
 }
 
-function inicioCiclos (Ciclos: CiclosMenstruales, idusuario: number) : void {
-    let dias: Date []= []
-    let dia: Date | null = null
+function inicioCiclos (Ciclos: CiclosMenstruales, idusuario: number) : number [] {
+    let dias: number []= []
+    let dia: Date | number | null = null
+    let tiempo: number = 0
     for (let i:number =0; i<Ciclos.length; i++) {
         if (Ciclos[i]?.id===idusuario) {
-            dia = new Date (Ciclos[i]?.inicio as string)
-            dias.push(dia as Date)
+            dia = new Date (Ciclos[i]?.inicio as string) //Aca los dias que son tipo string en el json pasan a ser type Date
+            dia = dia.getTime () //los dias que estan en type date pasan ahora a ser el tiempo transcurrido desde el 1/1/1970
+            dias.push(dia as number)
         }
         else {}
     }
-    console.log (dias)
+    dias.sort ((a, b) => a-b) // Esta función lo que hace es que solo sort toma el array como string (0, 1, 2, 3 … 8, 9, a, b) por lo que el 10 quedaría adelante del 2, entonces al hacer (a,b) => a-b se fija con cada par de números consecutivos en el array y hace la resta, si el resultado es negativo (a < b), coloca a antes que b.Si el resultado es positivo (a > b), coloca b antes que a. Si es cero, los deja en la misma posición. Hasta que el array quede ordenado
+    return dias
+}
+
+function DuracionProximo (anteriores: number[]): number | null {
+    let proximo: number = 0
+    let proximaduracion: number | null = 0
+    for (let i:number =0; i<anteriores.length; i++) {
+        proximo = proximo + anteriores[i]!
+    }
+    if (anteriores.length>0) {
+        proximaduracion= proximo/anteriores.length
+        let sincoma= Math.floor(proximaduracion)
+        if (proximaduracion-sincoma<0.5) {
+            proximaduracion = sincoma
+        }
+        else {
+            proximaduracion = sincoma+1
+        }
+    }
+    else {
+        proximaduracion = null
+        console.log ("No hay ciclos anteriores")
+    }
+    return proximaduracion
 }
 
 let Contenido = fs.readFileSync('../../Data/ciclos.json', 'utf8')
 let Ciclos = JSON.parse(Contenido) as CiclosMenstruales
-
-inicioCiclos (Ciclos, 0)
+let id: number = 0
+let DuracionCiclos: number[]= duracionCiclos (Ciclos, id)
+let InicioCiclos: number []= inicioCiclos (Ciclos, id)
+console.log (DuracionCiclos)
+console.log (InicioCiclos)
+let proximoTiempo: number | null = DuracionProximo (DuracionCiclos)
+console.log (proximoTiempo)
